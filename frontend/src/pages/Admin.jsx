c9d45e9be95e8112ffab080263fdea7f595556a7
vishuRizz <vishurizz0@gmail.com>
2024-12-31 21:36:01 +0530

    basic ackend setup done

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from './Auth/Login'
 
 function Admin() {
   return (
     <>
-    Admin
+    
     </>
   )
 }
---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TransparentLoginForm from '../../components/AdminComponents/LoginForm';
+
+const LoginPage = () => {
+  return (
+    <div
+      className="h-screen w-screen bg-cover bg-center"
+    >
+      <TransparentLoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
