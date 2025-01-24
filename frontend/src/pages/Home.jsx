1599cba0aabbb9fead5c3de7a7ba1dcdc786de85
shantanukumar22 <justforworkandstudy@gmail.com>
2025-01-24 15:09:36 +0530

    uhh

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import Header from "../sections/Header.jsx";
 import Hero from "../sections/Hero.jsx";
 import Features from "../sections/Features.jsx";
 import Pricing from "../sections/Pricing.jsx";
-import Faq from "../sections/Faq.jsx";
+// import Faq from "../sections/Faq.jsx";
 import Testimonials from "../sections/Testimonials.jsx";
 import Footer from "../sections/Footer.jsx";
 import "../index.css";
-import InfiniteHorizontalScroll from "../sections/SocialMedia.jsx";
-import SocialCardCarousel from "../sections/SocialMedia.jsx";
+// import InfiniteHorizontalScroll from "../sections/SocialMedia.jsx";
+// import SocialCardCarousel from "../sections/SocialMedia.jsx";
 import InfiniteMovingCards from "../sections/SocialMedia.jsx";
 // import HorizontalScroll from "../sections/SocialMedia.jsx";
 function Home() {
